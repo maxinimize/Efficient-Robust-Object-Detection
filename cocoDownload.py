e1772496ada64118436678d5ac126dd224e0a90c
Anton Liu <liuantonliu@gmail.com>
2024-03-18 21:35:40 -0400

    reorg

--- cocoDownload.py
+++
@@ -1,7 +0,0 @@
-import fiftyone as fo
-import fiftyone.zoo as foz
-
-dataset = foz.load_zoo_dataset("quickstart")
-session = fo.launch_app(dataset, desktop=1)
-
-# paste this all in terminal after python command
