36081f2516739b400fda6ae656e5ddd4f0fef0a6
Anton Liu <liuantonliu@gmail.com>
2024-03-23 20:56:14 -0400

    working FGSM with loss and image output

--- attacks/FGSM.py
+++ attacks/FGSM.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn.functional as F
 from attacks.attacker import Attacker
+from utils.loss import compute_loss
 
 class FGSM(Attacker):
-    def __init__(self, model, config, target=None):
-        super(FGSM, self).__init__(model, config)
-        self.target = target
+    def __init__(self, model, config=None, target=None, epsilon=2):
+        super(FGSM, self).__init__(model, config, epsilon)
+        self.target = target # target class
 
     def forward(self, x, y):
         """
@@ -14,28 +15,30 @@ class FGSM(Attacker):
         :param target : Target label 
         :return adversarial image
         """
-        x_adv = x.detach().clone()
+        # self.model.eval()
+        self.model.train() # has to be in train, or the model output dims change
+        # if self.config['random_init']:
+        #     x = self._random_init(x)
 
-        if self.config['random_init']:
-            x_adv = self._random_init(x_adv)
-
-        x_adv.requires_grad=True
-        self.model.zero_grad()
-
-        logit = self.model(x_adv)
-
-        if self.target is None:
-            #
-            cost = -F.cross_entropy(logit, y)
-        else:
-            cost = F.cross_entropy(logit, self.target)
+        x.requires_grad=True
+        logit = self.model(x)
+        loss, loss_components = compute_loss(logit, y, self.model)
         
-        if x_adv.grad is not None:
-            x_adv.grad.data.fill_(0)
-        cost.backward()
+        # if self.target is None:
+        #     #
+        #     cost = -F.cross_entropy(logit, y)
+        # else:
+        #     cost = F.cross_entropy(logit, self.target)
+        
+        if x.grad is not None:
+            x.grad.data.fill_(0)
 
-        x_adv.grad.sign_()
-        x_adv = x_adv - self.config['eps']*x_adv.grad
-        x_adv = torch.clamp(x_adv,*self.clamp)
+        self.model.zero_grad()
+        loss.backward()
+        
+        with torch.no_grad():
+            gradients_sign = x.grad.sign() # Collect the sign of the gradients
+            x_adv = x + self.epsilon * gradients_sign # Generate perturbed image
+            x_adv = torch.clamp(x_adv, 0, 1) # Clip the perturbed image to ensure it stays within valid pixel range
 
         return x_adv
